c642f7d7464f1d084f1d90036550a4d08d8b53ef
NandySree <nandysree98@gmail.com>
2018-09-16 09:26:52 +0530

    initial commit

---
+++ javaproject.java
@@ -0,0 +1,15 @@
+package javaproject;
+
+
+import javax.swing.JOptionPane;
+public class Javaproject {
+
+   
+    public static void main(String[] args) {
+      String output;
+      output = "welcome to my dialog box";
+      JOptionPane.showMessageDialog(null,output,"whatapp dude",JOptionPane.INFORMATION_MESSAGE);
+      System.exit(0);
+      
+    }
+}
